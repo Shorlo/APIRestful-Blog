703703b3d31135b31b0b7c94dde897917699ea92
Shorlo <shorlo.sbg@gmail.com>
2023-03-23 00:34:46 +0100

    create NodeJS server with express cors and connected to the database

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const { connection } = require('./database/connection');
+const express = require('express');
+const cors = require('cors');
 
 console.log("Starting APIRestful-Blog... ");
 
 // Connect to the database
 connection();
+
+// Create Node server
+const app = express();
+const PORT = 3900;
+
+// Config cors
+app.use(cors());
+
+// Convert body to ObjectJS
+app.use(express.json());
+
+// Create routes
+app.listen(PORT, () =>
+{
+    console.log(`Server running at Port: ${PORT}`);
+});
