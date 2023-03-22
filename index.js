a0f597acb3b7648b034bfad8463c76e3197e1675
Shorlo <shorlo.sbg@gmail.com>
2023-03-23 00:53:50 +0100

    creating and testing routes get endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { connection } = require('./database/connection');
 const express = require('express');
 const cors = require('cors');
+const { request, response } = require('express');
 
 console.log("Starting APIRestful-Blog... ");
 
@@ -17,8 +18,35 @@ app.use(cors());
 // Convert body to ObjectJS
 app.use(express.json());
 
+// Routes
+app.get("/", (request, response) =>
+{
+    console.log("Ejecuting test endpoint...");
+    return response.status(200).send(`
+        <h1>APIRestful with NodeJS</h1>
+    `);
+});
+app.get("/test", (request, response) =>
+{
+    console.log("Ejecuting test endpoint...");
+    return response.status(200).json(
+        [
+            {
+                title: "APIRestful NodeJS",
+                author: "Javier Sainz de Baranda",
+            },
+            {
+                title: "Fundaments of Object-C",
+                author: "Javier Sainz de Baranda",
+            }
+        ]
+    );
+});
+
 // Create routes
 app.listen(PORT, () =>
 {
     console.log(`Server running at Port: ${PORT}`);
 });
+
+
